7f6186779d73bdfa7c3a4ad4964dbc4470e4b3b4
David Lienhard <david.lienhard@tourasia.ch>
2021-04-12 06:38:20 +0200

    remove version numbers in docblocks remove `@version` & `@since` annotations from docblocks

--- src/CronInterface.php
+++ src/CronInterface.php
@@ -4,8 +4,6 @@
  *
  * @package         Cron
  * @author          David Lienhard <david.lienhard@tourasia.ch>
- * @version         1.0.1, 17.11.2020
- * @since           1.0.0, 16.11.2020, created
  * @copyright       tourasia
  */
 
@@ -18,8 +16,6 @@ namespace DavidLienhard\Cron;
  * allows to check if a given Cron-expression is due
  *
  * @author          David Lienhard <david.lienhard@tourasia.ch>
- * @version         1.0.0, 16.11.2020
- * @since           1.0.0, 16.11.2020, created
  * @copyright       tourasia
  */
 interface CronInterface
@@ -29,8 +25,6 @@ interface CronInterface
      * accepts an optional date to be checked against. default is now
      *
      * @author          David Lienhard <david.lienhard@tourasia.ch>
-     * @version         1.0.0, 16.11.2020
-     * @since           1.0.0, 16.11.2020, created
      * @copyright       tourasia
      * @param           array           $data           the Cron expression to be checked
      * @param           string          $date           an optional date
